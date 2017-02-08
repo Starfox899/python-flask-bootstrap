7f77a6debe62038a2fbef4aaea0816af92162039
Sascha Herrmann <sascha.herrmann@web.de>
2017-02-08 08:09:36 +0100

    Working version, delivered by gunicorn with multiple workers in parallel.

--- app/app.py
+++ app/app.py
@@ -1,15 +1,18 @@
-# app.py
 from flask import Flask
-from flask import request, render_template
-from config import BaseConfig
-
+from flask import render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
-app.config.from_object(BaseConfig)
+Bootstrap(app)
 
-@app.route('/', methods=['GET'])
-def list_all_data():
-    return "Hello World"
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
 
-if __name__ == '__main__':
-    app.run()
+@app.route('/hello.html')
+def hello():
+    nachricht = "Hallo Welt!"
+    return render_template('hello.html', nachricht=nachricht)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080, debug=True)
